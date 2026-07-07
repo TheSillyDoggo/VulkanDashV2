d91eb87814795bb68dd62f38f0b2bf20a4692fee
aster <talgood09@gmail.com>
2026-07-07 16:52:46 +1000

    fix

--- src/Utils/macOS.mm
+++ src/Utils/macOS.mm
@@ -1,3 +1,6 @@
+#include <Geode/platform/cplatform.h>
+#ifdef GEODE_IS_MACOS
+
 #include "macOS.hpp"
 
 #import <Foundation/Foundation.h>
@@ -8,3 +11,12 @@ void* getNSWindowHandle()
     NSArray<NSWindow*>* windows = [NSApplication sharedApplication].windows;
     return windows[0];
 }
+
+#else
+
+void* getNSWindowHandle()
+{
+    return nullptr;
+}
+
+#endif
