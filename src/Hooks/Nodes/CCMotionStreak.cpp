f7d875ec99e867ed315e55b8f2c1fd5e06a5240e
thesillydoggo <talgood09@gmail.com>
2026-07-08 17:30:52 +1000

    wip

---
+++ src/Hooks/Nodes/CCMotionStreak.cpp
@@ -0,0 +1,13 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/CCMotionStreak.hpp>
+#include <bgfx/bgfx.h>
+#include "../Utils/VertexLayoutManager.hpp"
+#include "../Utils/ShaderCache.hpp"
+#include <BGFX.hpp>
+#include "../CCTexture2D.hpp"
+
+using namespace geode::prelude;
+
+class $modify (BGFXMotionStreak, CCMotionStreak)
+{
+};
